5422905b66392b5a43cec9f67da3018a8af6d997
Alex Ling <hkalexling@gmail.com>
2020-09-13 21:28:24 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,17 @@
 
 cd docs
 rm -rf Readme Wiki
+
 git clone https://github.com/hkalexling/Mango
 mkdir Readme
 cp Mango/README.md Readme
 cp -r Mango/public Readme
 rm -rf Mango
+
 git clone https://github.com/hkalexling/Mango.wiki Wiki
+mv Wiki/Home.md Wiki/README.md
+
 rm -rf Readme/.git Wiki/.git
+
 cd ..
 python3 wiki.py
